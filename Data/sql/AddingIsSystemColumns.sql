b4468c70cbc9c12aff294aeca122d18a8bc87988
LargeSquare <danilyuk.serg88@gmail.com>
2021-02-09 18:29:59 +0300

    Add strings resource to app and check-columns to DB

---
+++ Data/sql/AddingIsSystemColumns.sql
@@ -0,0 +1,22 @@
+alter table [BudgetState] add
+	[IsSystem] bit default 0 not null;
+go
+
+alter table [TypeOfBudgetAction] add
+	[IsSystem] bit default 0 not null;
+go
+
+alter table [TypeOfEvent] add
+	[IsSystem] bit default 0 not null;
+go
+
+alter table [TypeOfDelivery] add
+	[IsSystem] bit default 0 not null;
+go
+
+alter table [TypeOfSale] add
+	[IsSystem] bit default 0 not null;
+go
+
+alter table [Event] drop column [IsSystem];
+go
